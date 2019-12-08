047b9c8561b5d44515603d742af5a3880faa6ec2
ddhp <sevenknee@gmail.com>
2019-12-08 23:43:55 +0900

    client done

--- src/LazyLoadClient.js
+++ src/LazyLoadClient.js
@@ -1,51 +1,71 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, {
+  useState, useEffect, useRef, useCallback,
+} from 'react';
+import PropTypes from 'prop-types';
 
-function LazyLoadClient({ children, width, height, offset = window.innerHeight}) {
-    console.log(width, height, offset);
-    const [isVisible, setIsVisible] = useState(false);
-    const el = useRef(null);
+export function LazyLoadClient({
+  children, width = '100%', height = '100%', offset = 0,
+}) {
+  // console.log(width, height, offset);
+  const [isVisible, setIsVisible] = useState(false);
+  const el = useRef(null);
 
-    const onScroll = useCallback(
-        () => {
-            console.log('onscroll executed');
-            if (!el || !el.current) {
-                return;
-            }
-            if ((window.innerHeight + window.pageYOffset + offset) > el.current.offsetTop) {
-                setIsVisible(true);
-            }
-        },
-        [offset],
+  const onScroll = useCallback(
+    () => {
+      // console.log('onscroll executed');
+      if (!el || !el.current) {
+        return;
+      }
+      if ((window.innerHeight + window.pageYOffset + offset)
+        > el.current.getBoundingClientRect().top
+      ) {
+        // console.log(window.innerHeight, window.pageYOffset, window.innerHeight,
+        //   el.current.offsetTop);
+        setIsVisible(true);
+      }
+    },
+    [],
+  );
+
+  useEffect(() => {
+    onScroll();
+  }, [onScroll]);
+
+  useEffect(() => {
+    if (isVisible) {
+      window.removeEventListener('scroll', onScroll);
+    } else {
+      window.addEventListener('scroll', onScroll);
+    }
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [isVisible, onScroll]);
+
+  return isVisible
+    ? children
+    : (
+      <div
+        style={{
+          width,
+          height,
+        }}
+        ref={el}
+      />
     );
-
-    const onDestroy = useCallback(
-        () => {
-            console.log('onDestroy executed');
-            window.removeEventListener('scroll', onScroll);
-        },
-        [onScroll],
-    );
-
-    useEffect(() => {
-        console.log('useEFfect executed');
-        onScroll();
-        if (isVisible) {
-            onDestroy();
-            return () => {};
-        }
-        window.addEventListener('scroll', onScroll);
-        return onDestroy;
-    }, [isVisible, onScroll, onDestroy]);
-
-    return isVisible ?
-        children :
-        <div 
-            style={{
-                width,
-                height,
-            }}
-            ref={el}
-        />;
 }
+
+LazyLoadClient.propTypes = {
+  children: PropTypes.element.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  offset: PropTypes.number,
+};
+
+LazyLoadClient.defaultProps = {
+  width: '100%',
+  height: '100%',
+  offset: 0,
+};
 
 export default LazyLoadClient;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import LazyLoadClient from './LazyLoadClient';
+// import PropTypes from 'prop-types';
+import ExecutionEnvironment from 'exenv';
+import { LazyLoadClient } from './LazyLoadClient';
 
-const ExecutionEnvironment = require('exenv');
 
-export const LazyLoad = ({ children, ...props }) => {
-    return ExecutionEnvironment.canUseViewport ?
-        <LazyLoadClient
-            {...props}
-        >
-            {children}
-        </LazyLoadClient>
-        : children
-};
+// export const LazyLoad = () => ((
+//   <div>test</div>
+// ));
+
+console.log(ExecutionEnvironment.canUseViewport);
+export const LazyLoad = ({ children, ...props }) => (ExecutionEnvironment.canUseViewport
+  ? (
+    <LazyLoadClient
+      {...props}
+    >
+      {children}
+    </LazyLoadClient>
+  )
+  : children);
+//
+// LazyLoad.propTypes = {
+//   children: PropTypes.element.isRequired,
+// };
 
 export default LazyLoad;
